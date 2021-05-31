66a4f601538d164a2ac685d3d072d8fb58a517e3
HanBI24 <alsltjdrhs@naver.com>
2021-06-01 02:25:58 +0900

    Add setting screen

--- lib/screen/main/estate_screen.dart
+++ lib/screen/main/estate_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:jikbang_refactoring/screen/filterlist/filterlist_screen.dart';
 import 'package:jikbang_refactoring/screen/second/second_screen.dart';
+import 'package:jikbang_refactoring/screen/settings/setting_screen.dart';
 
 class RealEstate extends StatelessWidget {
   @override
@@ -9,19 +10,19 @@ class RealEstate extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.black12,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.only(left: 30.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    '어떤 집을 찾고 계세요?',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  Container(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.only(left: 30.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  '어떤 집을 찾고 계세요?',
+                  style: TextStyle(fontSize: 20),
+                ),
+                Container(
                     alignment: Alignment.centerRight,
                     child: Row(
                       children: <Widget>[
@@ -31,136 +32,129 @@ class RealEstate extends StatelessWidget {
                         GestureDetector(
                           child: Icon(Icons.settings),
                           onTap: () {
-
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => SettingScreen()));
                           },
                         )
                       ],
-                    )
-                  )
+                    ))
+              ],
+            ),
+          ),
+          Column(
+            children: <Widget>[
+              GridView.count(
+                crossAxisCount: 2,
+                shrinkWrap: true,
+                primary: false,
+                childAspectRatio: 1.3,
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.all(10.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.white,
+                          onPrimary: Colors.black,
+                          shape: const RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10)))),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Column(
+                            children: <Widget>[
+                              Text('아파트', style: TextStyle(fontSize: 28)),
+                              Text('매매/전, 월세\n신축분양')
+                            ],
+                          ),
+                        ],
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(10.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          alignment: Alignment.centerLeft,
+                          primary: Colors.white,
+                          onPrimary: Colors.black,
+                          shape: const RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10)))),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Column(
+                            children: <Widget>[
+                              Text('빌라    ', style: TextStyle(fontSize: 28)),
+                              Text('신축분양/매매\n전, 월세')
+                            ],
+                          ),
+                        ],
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(10.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.white,
+                          onPrimary: Colors.black,
+                          shape: const RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10)))),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Column(
+                            children: <Widget>[
+                              Text('원룸', style: TextStyle(fontSize: 28)),
+                              Text('전, 월세'),
+                            ],
+                          ),
+                        ],
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SecondScreen()));
+                      },
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(10.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.white,
+                          onPrimary: Colors.black,
+                          shape: const RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10)))),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                          Column(
+                            children: <Widget>[
+                              Text('오피스텔', style: TextStyle(fontSize: 28)),
+                              Text('도시형생활주택\n전, 월세')
+                            ],
+                          ),
+                        ],
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
                 ],
               ),
-            ),
-            Column(
-              children: <Widget>[
-                GridView.count(
-                  crossAxisCount: 2,
-                  shrinkWrap: true,
-                  primary: false,
-                  childAspectRatio: 1.3,
-                  children: <Widget>[
-                      Container(
-                        padding: const EdgeInsets.all(10.0),
-                        child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            primary: Colors.white,
-                            onPrimary: Colors.black,
-                            shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10)))
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: <Widget>[
-                            Column(
-                              children: <Widget>[
-                                Text(
-                                    '아파트',
-                                    style: TextStyle(fontSize: 28)
-                                ),
-                                Text('매매/전, 월세\n신축분양')
-                              ],
-                            ),
-                          ],
-                        ),
-                        onPressed: (){},
-                        ),
-                      ),
-                    Container(
-                      padding: const EdgeInsets.all(10.0),
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          alignment: Alignment.centerLeft,
-                            primary: Colors.white,
-                            onPrimary: Colors.black,
-                            shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10)))
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: <Widget>[
-                            Column(
-                              children: <Widget>[
-                                Text(
-                                    '빌라    ',
-                                    style: TextStyle(fontSize: 28)
-                                ),
-                                Text('신축분양/매매\n전, 월세')
-                              ],
-                            ),
-                          ],
-                        ),
-                        onPressed: (){},
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.all(10.0),
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            primary: Colors.white,
-                            onPrimary: Colors.black,
-                            shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10)))
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: <Widget>[
-                            Column(
-                              children: <Widget>[
-                                Text(
-                                    '원룸',
-                                    style: TextStyle(fontSize: 28)
-                                ),
-                                Text('전, 월세'),
-                              ],
-                            ),
-                          ],
-                        ),
-                        onPressed: (){
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => SecondScreen())
-                          );
-                        },
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.all(10.0),
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            primary: Colors.white,
-                            onPrimary: Colors.black,
-                            shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10)))
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: <Widget>[
-                            Column(
-                              children: <Widget>[
-                                Text(
-                                    '오피스텔',
-                                    style: TextStyle(fontSize: 28)
-                                ),
-                                Text('도시형생활주택\n전, 월세')
-                              ],
-                            ),
-                          ],
-                        ),
-                        onPressed: (){},
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            )
-          ],
-        )
-      ),
+            ],
+          )
+        ],
+      )),
     );
   }
 }
---
+++ lib/screen/settings/setting_screen.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SettingScreen extends StatefulWidget {
+  @override
+  _SettingScreen createState() => _SettingScreen();
+
+}
+
+class _SettingScreen extends State<SettingScreen> {
+  bool isSwitched = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(15.0),
+            child: Row(
+              children: <Widget>[
+                Icon(Icons.keyboard_backspace),
+                Text(
+                  '설정',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(border: Border.all(color: Colors.black12)),
+            padding: const EdgeInsets.fromLTRB(20, 10, 10, 10),
+            child: Row(
+              children: <Widget>[
+                Text(
+                  '내 정보',
+                  style: TextStyle(fontSize: 20),
+                ),
+                Container(
+                    width: 280,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[Icon(Icons.chevron_right)],
+                    ))
+              ],
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(border: Border.all(color: Colors.black12)),
+            padding: const EdgeInsets.fromLTRB(20, 10, 10, 10),
+            child: Row(
+              children: <Widget>[
+                Text(
+                  '약관 및 정책',
+                  style: TextStyle(fontSize: 20),
+                ),
+                Container(
+                    width: 240,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[Icon(Icons.chevron_right)],
+                    ))
+              ],
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(border: Border.all(color: Colors.black12)),
+            padding: const EdgeInsets.fromLTRB(20, 10, 10, 10),
+            child: Row(
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Container(
+                      width: 240,
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          '알림 받기',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                    ),
+                    Text('뉴스 및 이벤트, 관심 아파트 소식 등 직방\n서비스와 관련된 알림을 받을 수 있습니다.')
+                  ],
+                ),
+                Container(
+                  width: 110,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        Switch(value: isSwitched, onChanged: (value) {
+                          setState(() {
+                            isSwitched = value;
+                          });
+                        },
+                        activeColor: Colors.orange,
+                        )
+                      ],
+                    )
+                )
+              ],
+            ),
+          )
+        ],
+      )),
+    );
+  }
+}
